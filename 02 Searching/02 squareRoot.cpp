694385f735141f77b5388b84d3c1f4b9d2679b57
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-08-10 23:25:06 +0530

    TC: logN using Binary Search because of monotonicity

---
+++ 02 Searching/02 squareRoot.cpp
@@ -0,0 +1,27 @@
+
+// Function to find square root
+// x: element to find square root
+long long int floorSqrt(long long int x) 
+{
+    
+    if(x == 0 || x == 1)
+        return x;
+        
+    long long int low = 0, high = x, ans = 1;
+    
+    while (low <= high){
+        long long int mid = (low + high)/2;
+        
+        if (mid*mid == x)
+            return mid;
+        if (mid*mid < x){
+            low = mid+1;
+            ans = mid;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    
+    return ans;
+}
