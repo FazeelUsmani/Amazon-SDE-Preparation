4e1e0c02e08dceda5982c654aec5e900e3385f87
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-01-02 23:00:16 +0530

    updating and returning a vector

--- 04 Matrix/07 diagonalmatrix.cpp
+++ 04 Matrix/07 diagonalmatrix.cpp
@@ -1,19 +1,14 @@
   
-void MatrixDiagonally(int mat[][100], int n) 
+vector<int> MatrixDiagonally(int mat[][100], int n) 
 { 
-
     int i = 0, j = 0; 
-  
-
     bool Upward = true; 
-  
-    
+    vector<int> ans;
+ 
     for (int k = 0; k < n * n;) { 
-        
-       
         if (Upward) { 
             for (; i >= 0 && j < n; j++, i--) { 
-                cout << mat[i][j] << " "; 
+                ans.push_back(mat[i][j]);
                 k++; 
             } 
   
@@ -21,22 +16,18 @@ void MatrixDiagonally(int mat[][100], int n)
                 i = 0; 
             if (j == n) 
                 i = i + 2, j--; 
-        } 
-  
-   
+        }   
         else { 
             for (; j >= 0 && i < n; i++, j--) { 
-                cout << mat[i][j] << " "; 
+                ans.push_back(mat[i][j]);
                 k++; 
             } 
-  
             if (j < 0 && i <= n - 1) 
                 j = 0; 
             if (i == n) 
                 j = j + 2, i--; 
         } 
-  
-
         Upward = !Upward; 
     } 
+  return ans;
 } 
