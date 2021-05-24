65cd3f7de5cdc78bb929d9e673d2aa41e2966a44
JurajZitnansky <zitnansky8@uniba.sk>
2021-05-24 22:30:07 +0200

    Update 10 quadraticProbing.cpp
    Forgot to initialize vector<int> hash to -1s.

--- 05 Hashing/10 quadraticProbing.cpp
+++ 05 Hashing/10 quadraticProbing.cpp
@@ -1,6 +1,9 @@
 void QuadraticProbing(vector <int>&hash, int hashSize, int arr[], int N){
     int position;
     int counter = 1;
+    for(int i=0; i<hashSize; i++){
+        hash[i] = -1;
+    }
     for (int i = 0; i < N; i++){
         position = arr[i] % hashSize;
         while (hash[position] != (-1) && counter < hashSize){
