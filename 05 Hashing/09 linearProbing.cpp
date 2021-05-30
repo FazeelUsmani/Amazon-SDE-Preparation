0cbc11f4a1c551b4ce407ddba33c8c91a9233fee
JurajZitnansky <zitnansky8@uniba.sk>
2021-05-30 18:46:41 +0200

    Update 09 linearProbing.cpp
    Hopefully this will fix the problem. Before I have never checked if the element is already in a list and i added it. Now if the element that i want to add is already in a list I drop it.

--- 05 Hashing/09 linearProbing.cpp
+++ 05 Hashing/09 linearProbing.cpp
@@ -1,19 +1,28 @@
 vector<int> linearProbing(int hashSize, int arr[], int N){
     vector<int> output(hashSize);
+    bool inserted = false;
+    int counter = 0;
     for (unsigned i=0; i<hashSize; i++){
         output[i]=-1;
     }
     int position;
     for(int i=0; i<N; i++){
-        if(i>=hashSize){
+        if(counter>=hashSize){
             break;
         }
         position = arr[i]%hashSize;
         while (output[position] != (-1)){
+            if (output[position] == arr[i]){
+                inserted = true;
+                break;
+            }
             position=(position+1)%hashSize;
-
         }
-        output[position] = arr[i];
+        if(inserted == false) {
+            output[position] = arr[i];
+            counter++;
+        }
+        inserted = false;
     }
     return output;
 }
